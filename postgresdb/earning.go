8b9bf81bdf3cf0058748f37000ba000dfd4d88b2
Yigithan Colak <yigithancolak97@gmail.com>
2023-10-26 17:01:02 +0300

    fix: Date ranges added on earnings listing

--- postgresdb/earning.go
+++ postgresdb/earning.go
@@ -10,22 +10,25 @@ SELECT
     COALESCE(SUM(CASE WHEN p.currency = 'try' THEN p.amount ELSE 0 END), 0) AS total_amount_try,
     COALESCE(SUM(CASE WHEN p.currency = 'usd' THEN p.amount ELSE 0 END), 0) AS total_amount_usd,
     COALESCE(SUM(CASE WHEN p.currency = 'eur' THEN p.amount ELSE 0 END), 0) AS total_amount_eur,
-	COUNT(*) OVER() as total_count
+    COUNT(*) OVER() as total_count
 FROM
     org_groups g
 JOIN
     payments p ON g.id = p.org_group_id
 WHERE
     g.organization_id = $1
+AND
+    p.date BETWEEN $2 AND $3
 GROUP BY
     g.id
 ORDER BY 
     g.name`
 
 	var args []interface{}
-	args = append(args, orgID)
+	args = append(args, orgID, startDate, endDate)
+
 	if limit != nil && offset != nil {
-		query += ` LIMIT $2 OFFSET $3`
+		query += ` LIMIT $4 OFFSET $5`
 		args = append(args, limit, offset)
 	}
 
